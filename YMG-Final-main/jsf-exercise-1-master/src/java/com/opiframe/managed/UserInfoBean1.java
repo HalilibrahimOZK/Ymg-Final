7dc4a1576d74b9da0526624a3e484e6d24ab5a1f
HalilibrahimOZK <121833386+HalilibrahimOZK@users.noreply.github.com>
2024-07-03 14:36:23 +0300

    Add files via upload

---
+++ YMG-Final-main/jsf-exercise-1-master/src/java/com/opiframe/managed/UserInfoBean1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.opiframe.managed;
+
+import javax.inject.Named;
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ *
+ * @author Gokce
+ */
+@Named(value = "userInfoBean1")
+@ApplicationScoped
+public class UserInfoBean1 {
+
+    /**
+     * Creates a new instance of UserInfoBean1
+     */
+    public UserInfoBean1() {
+    }
+    
+}
